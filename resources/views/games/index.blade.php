6adf31d9d2ac6165e0459012eb1d71a62c8e4104
Aaron <aaronmacmillan25@gmail.com>
2026-09-23 19:18:34 +0200

    Blade directives role en can

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -1,57 +1,62 @@
-
 @extends('base')
 
 @section('title', '🎮 Game Collection')
 
 @section('content')
+
+
+@can('product invoeren')
     <a href="/games/create" class="btn btn-success mb-3">🎮 Add Game</a>
+@endcan
 
-    <table class="table">
-        <thead class="thead-dark">
+<table class="table">
+    <thead class="thead-dark">
+        <tr>
+            <th>ID</th>
+            <th>Game</th>
+            <th>Platform</th>
+            <th>Rating</th>
+            <th>Genre</th>
+            <th>Show</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        {{-- 1. Begin de som op nul --}}
+        @php( $sum = 0 )
+
+        @foreach($games as $game)
+
+            {{-- 2. Tel de rating van elke game op --}}
+            @php( $sum += $game->rating )
+
             <tr>
-                <th>ID</th>
-                <th>Game</th>
-                <th>Platform</th>
-                <th>Rating</th>
-                <th>Genre</th>
-                <th>Show</th>
-                <th>Edit</th>
-                <th>Delete</th>
-            </tr>
-        </thead>
+                <td>{{ $game->id }}</td>
+                <td>{{ $game->game_name }}</td>
+                <td>{{ $game->platform }}</td>
+                <td>{{ $game->rating }}/10</td>
+                <td>{{ $game->genre }}</td>
 
-        <tbody>
-            {{-- 1. Begin de som op nul --}}
-            @php( $sum = 0 )
+                <td>
+                    <a href="/games/show/{{ $game->id }}"
+                       class="btn btn-info btn-sm">
+                        Show
+                    </a>
+                </td>
 
-            @foreach($games as $game)
-
-                {{-- 2. Tel de rating van elke game op --}}
-                @php( $sum += $game->rating )
-
-                <tr>
-                    <td>{{ $game->id }}</td>
-                    <td>{{ $game->game_name }}</td>
-                    <td>{{ $game->platform }}</td>
-                    <td>{{ $game->rating }}/10</td>
-                    <td>{{ $game->genre }}</td>
-                    
-                    <td>
-                        <a           
-                           href="/games/show/{{ $game->id }}"
-                            class="btn btn-info btn-sm">
-                            Show
-                        </a>
-                    </td>
-                     
-                    <td>
+                <td>
+                    @can('product aanpassen')
                         <a href="/games/edit/{{ $game->id }}"
                            class="btn btn-primary btn-sm">
                             Edit
                         </a>
-                    </td>
+                    @endcan
+                </td>
 
-                    <td>
+                <td>
+                    @can('product verwijderen')
                         <form action="/games/destroy/{{ $game->id }}"
                               method="post">
                             @csrf
@@ -62,22 +67,25 @@
                                 Delete
                             </button>
                         </form>
-                    </td>
-                </tr>
-            @endforeach
-
-            {{-- 3. Toon het gemiddelde onder de games --}}
-            <tr>
-                <td colspan="4">
-                    <strong>Gemiddelde rating:</strong>
+                    @endcan
                 </td>
-                <td>
-                    <strong>
-                        {{ count($games) > 0 ? number_format($sum / count($games), 1) : 0 }}/10
-                    </strong>
-                </td>
-                <td></td>
             </tr>
-        </tbody>
-    </table>
+        @endforeach
+
+        {{-- 3. Toon het gemiddelde onder de games --}}
+        <tr>
+            <td colspan="4">
+                <strong>Gemiddelde rating:</strong>
+            </td>
+            <td>
+                <strong>
+                    {{ count($games) > 0 ? number_format($sum / count($games), 1) : 0 }}/10
+                </strong>
+            </td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
+
+
 @endsection
